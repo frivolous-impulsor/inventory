c92b56c759050c8882d563b47dc18854cc4537f2
frivolous-impulsor <ultrapilot95@gmail.com>
2024-07-31 16:20:40 -0400

    cursor set up

--- tableOperations.py
+++ tableOperations.py
@@ -1,13 +1,22 @@
 from getpass import getpass
 from mysql.connector import connect, Error
 
+
+
 try:
     with connect(
         host="localhost",
-        user=input("ener username: "),
-        password = getpass("enter password: "),
+        user = "root",
+        password = "R05yste3Ad31n",
+        database = "rodevices"
     ) as connection:
-        print(connection)
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM staff;")
+            results = cursor.fetchall()
+            for result in results:
+                print(result)
+
+
 
 except Error as e:
     print(e)
